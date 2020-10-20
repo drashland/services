ffcd472ea0caf800d887930741e725386a79f81c
Eric Crooks <crookse@users.noreply.github.com>
2020-10-20 19:10:09 -0400

    add commandExists()

--- cli/command_service.ts
+++ cli/command_service.ts
@@ -1,12 +1,19 @@
 import { logError } from "./logger_service.ts";
 
-export function commandRequiresArgs(command: string, args: string[]): void {
-  if (args.length == 1) {
-    logError(`The \`${command}\` command requires an argument.`);
+export function commandExists(commands: string[], command: string) {
+  if (commands.indexOf(command) == -1) {
+    logError(`Command \`${command}\` not recognized.`);
     Deno.exit();
   }
 }
 
 export function commandIs(command: string): boolean {
   return Deno.args[0] == command;
+}
+
+export function commandRequiresArgs(command: string, args: string[]): void {
+  if (args.length == 1) {
+    logError(`The \`${command}\` command requires an argument.`);
+    Deno.exit();
+  }
 }
