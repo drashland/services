49d8190257afe679392d00020dcc41a1535d3f58
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-05-31 13:36:50 +0100

    chore: Move rhum into tests deps to keep out of main dep tree (#22)
    * chore: Move rhum into tests deps to keep out of main dep tree
    
    * deno fmt

--- tests/index/index_service_test.ts
+++ tests/index/index_service_test.ts
@@ -1,4 +1,4 @@
-import { Rhum } from "../../deps.ts";
+import { Rhum } from "../deps.ts";
 import { IndexService, ISearchResult } from "../../index/index_service.ts";
 
 const lookupTable: Map<number, string> = new Map();
