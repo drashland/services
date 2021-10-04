61dcefc6ae719102b141aa65c48b9b2c1dd2ec0e
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-10-04 18:25:16 +0100

    BREAKING: remove logger

---
+++ ci/deps.ts
@@ -0,0 +1,2 @@
+import { ConsoleLogger } from "https://deno.land/x/unilogger@v1.0.1/mod.ts";
+export const consoleLogger = new ConsoleLogger({});
--- mod.ts
+++ mod.ts
@@ -1,4 +1,2 @@
 export { BumperService } from "./ci/bumper_service.ts";
 export { CliService } from "./cli/cli_service.ts";
-export { ConsoleLogger } from "./loggers/console_logger.ts";
-export { FileLogger } from "./loggers/file_logger.ts";
